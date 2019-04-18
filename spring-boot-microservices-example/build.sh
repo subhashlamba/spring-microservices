989a5d3ba356e6ae426484b1ad96560bd59c81d7
Subhash Lamba <subhashlammba@gmail.com>
2019-04-18 23:43:26 +0530

    Update build.sh

--- spring-boot-microservices-example/build.sh
+++ spring-boot-microservices-example/build.sh
@@ -1,6 +1,6 @@
-mvn clean install -f .\spring-boot-cloud-eureka-server\pom.xml
-mvn clean install -f .\spring-boot-cloud-eureka-account-service\pom.xml
-mvn clean install -f .\spring-boot-cloud-zuul-routing\pom.xml
+mvn clean install -f ./spring-boot-cloud-eureka-server/pom.xml
+mvn clean install -f ./spring-boot-cloud-eureka-account-service/pom.xml
+mvn clean install -f ./spring-boot-cloud-zuul-routing/pom.xml
 
 java -jar spring-boot-cloud-eureka-server/target/eureka-server.jar &
 java -jar spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8181 &
