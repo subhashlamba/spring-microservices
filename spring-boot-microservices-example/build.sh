cb9d75d4f678d782af15662d9d3f096dc3c10464
Subhash Lamba <subhashlammba@gmail.com>
2019-04-18 23:53:38 +0530

    Update build.sh

--- spring-boot-microservices-example/build.sh
+++ spring-boot-microservices-example/build.sh
@@ -2,9 +2,9 @@ mvn clean install -f ./spring-boot-microservices-example/spring-boot-cloud-eurek
 mvn clean install -f ./spring-boot-microservices-example/spring-boot-cloud-eureka-account-service/pom.xml
 mvn clean install -f ./spring-boot-microservices-example/spring-boot-cloud-zuul-routing/pom.xml
 
-java -jar spring-boot-microservices-example/spring-boot-cloud-eureka-server/target/eureka-server.jar &
-java -jar spring-boot-microservices-example/spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8181 &
-java -jar spring-boot-microservices-example/spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8182 &
-java -jar spring-boot-microservices-example/spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8183 &
-java -jar spring-boot-microservices-example/spring-boot-cloud-zuul-routing/target/zuul-router.jar --server.port=8080 & 
+java -jar spring-boot-microservices-example/spring-boot-cloud-eureka-server/target/eureka-server.jar 
+java -jar spring-boot-microservices-example/spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8181 
+java -jar spring-boot-microservices-example/spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8182 
+java -jar spring-boot-microservices-example/spring-boot-cloud-eureka-account-service/target/account-service.jar --server.port=8183 
+java -jar spring-boot-microservices-example/spring-boot-cloud-zuul-routing/target/zuul-router.jar --server.port=8080 
 
